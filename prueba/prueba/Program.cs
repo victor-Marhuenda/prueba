4668b78299b947fd8b2b04f0789ce50834979481
viworh <gusiluzman777.omg@gmail.com>
2020-10-19 12:23:29 +0200

    añadida variable

--- prueba/prueba/Program.cs
+++ prueba/prueba/Program.cs
@@ -4,9 +4,12 @@ namespace prueba
 {
     class Program
     {
+        static int n1;
         static void Main(string[] args)
         {
+            n1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hello there");
+            Console.WriteLine(n1);
         }
     }
 }
